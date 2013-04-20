ed9f1b36e920c63ee67e8b42dc2e6a579dc1cfd7
Tsin Smith <tsin.creator@gmail.com>
2013-04-20 18:16:26 +0800

    Add more configurations on c-mode
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/lang-pack/config/c-mode-conf.el
+++ lib/packs/lang-pack/config/c-mode-conf.el
@@ -4,9 +4,14 @@
 (setq ac-clang-flags
       (mapcar (lambda (item)(concat "-I" item))
               (split-string system-header-paths)))
-(defun ac-cc-mode-setup ()
+(defun cc-mode-setup ()
+  (c-set-style "k&r")
+  (c-toggle-auto-state)
+  (c-toggle-hungry-state)                               ;; Delete continuous spaces by backspace
+  (setq c-basic-offset 8)                               ;; A tab is 8-character wide
+  (which-function-mode)
   (setq ac-sources (append '(ac-source-clang) ac-sources)))
-(add-hook 'c-mode-hook 'ac-cc-mode-setup)
-(add-hook 'c++-mode-hook 'ac-cc-mode-setup)
+(add-hook 'c-mode-hook 'cc-mode-setup)
+(add-hook 'c++-mode-hook 'cc-mode-setup)
 
 (add-to-list 'ac-modes 'c-mode)
