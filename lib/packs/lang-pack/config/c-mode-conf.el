93352ef6d90b2090b448c7ca9310960a8c771ec9
Tsin Smith <tsin.creator@gmail.com>
2013-04-28 10:30:38 +0800

    Enable auto-complete for C/C++ only when requested
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/lang-pack/config/c-mode-conf.el
+++ lib/packs/lang-pack/config/c-mode-conf.el
@@ -1,20 +1,20 @@
 (config/load-config-file "c-mode-os-conf.el")
 
 (require 'auto-complete-clang)
-(setq ac-clang-flags
-      (mapcar (lambda (item)(concat "-I" item))
-	      (split-string system-header-paths)))
 (make-variable-buffer-local 'ac-clang-flags)
 
 (add-hook 'c-generic-project-file-visit-hook
 	  (lambda ()
-	    (setq ac-clang-flags
-		  (let* ((project-no-stdlib (eproject-attribute :no-stdlib))
-			 (system-include-dirs (if project-no-stdlib "" (split-string system-header-paths)))
-			 (project-include-dirs (split-string (eproject-attribute :include-dir))))
-		    (append
-		     (mapcar (lambda (item) (concat "-I" item)) system-include-dirs)
-		     (mapcar (lambda (item) (concat "-I" (eproject-root) item)) project-include-dirs))))))
+	    (when (eproject-attribute :enable-ac)
+	      (setq ac-clang-flags
+		    (let* ((project-no-stdlib (eproject-attribute :no-stdlib))
+			   (system-include-dirs (if project-no-stdlib "" (split-string system-header-paths)))
+			   (include-dir (eproject-attribute :include-dir))
+			   (project-include-dirs (if (eq include-dir nil) nil (split-string include-dir))))
+		      (append
+		       (mapcar (lambda (item) (concat "-I" item)) system-include-dirs)
+		       (mapcar (lambda (item) (concat "-I" (eproject-root) item)) project-include-dirs))))
+	      (setq ac-sources (append '(ac-source-clang) ac-sources)))))
 
 (defun c-mode-setup ()
   (c-set-style "k&r")
@@ -22,8 +22,7 @@
   (c-toggle-hungry-state)                               ;; Delete continuous spaces by backspace
   (setq c-basic-offset 8)                               ;; A tab is 8-character wide
   (which-function-mode)
-  (eproject-maybe-turn-on)
-  (setq ac-sources (append '(ac-source-clang) ac-sources)))
+  (eproject-maybe-turn-on))
 
 (add-hook 'c-mode-hook 'c-mode-setup)
 (add-hook 'c++-mode-hook 'c-mode-setup)
