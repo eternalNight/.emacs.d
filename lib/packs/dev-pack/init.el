233d846ac540dc8b3727d3511dbf36a4c8e3669e
Tsin Smith <tsin.creator@gmail.com>
2013-05-07 13:28:59 +0800

    Configure eshell before eproject
    This avoid eshell temporary files at the top level of the directory.
    
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/dev-pack/init.el
+++ lib/packs/dev-pack/init.el
@@ -1,6 +1,6 @@
 (config/load-config-file "auto-complete-conf.el")
+(config/load-config-file "eshell-conf.el")    ; Must invoked before eproject-conf
 (config/load-config-file "eproject-conf.el")
-(config/load-config-file "eshell-conf.el")
 (config/load-config-file "magit-conf.el")
 (config/load-config-file "rainbow-delimiters-mode-conf.el")
 (config/load-config-file "xcscope-conf.el")
