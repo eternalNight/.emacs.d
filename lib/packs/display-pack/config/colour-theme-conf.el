64a9d4f9d6f7136d901a61ab0fbf0e51ed69aea5
Tsin Smith <tsin.creator@gmail.com>
2013-05-28 20:07:52 +0800

    Set frame-background-mode as some mode may use it
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/display-pack/config/colour-theme-conf.el
+++ lib/packs/display-pack/config/colour-theme-conf.el
@@ -6,4 +6,7 @@
 (load-file (concat (config/pack-lib-dir) "gandalf.el"))
 
 (color-theme-cyberpunk)
+(custom-set-variables
+ '(frame-background-mode 'dark)                    ;; The color theme has a dark background
+)
 (set-cursor-color "yellow")
