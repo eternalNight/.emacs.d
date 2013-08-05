85c290caee4b58dd3bf650ca7ec8fbaf1c5b3e41
Tsin Smith <tsin.creator@gmail.com>
2013-08-05 19:35:36 +0800

    Add Alloy mode
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

---
+++ lib/packs/lang-pack/config/alloy-mode-conf.el
@@ -0,0 +1,2 @@
+(autoload 'alloy-mode "alloy-mode" "Mode for Alloy" t)
+(add-to-list 'auto-mode-alist '("\\.als$" . alloy-mode))
--- lib/packs/lang-pack/init.el
+++ lib/packs/lang-pack/init.el
@@ -1,3 +1,4 @@
+(config/load-config-file "alloy-mode-conf.el")
 (config/load-config-file "c-mode-conf.el")
 (config/load-config-file "clojure-conf.el")
 (config/load-config-file "quack-conf.el")
