b917b34caad6dd3d7ce7229601058b4b26a99560
Tsin Smith <tsin.creator@gmail.com>
2013-10-21 10:51:39 +0800

    Bind C-j to evaluation
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/reasoning-pack/config/proof-general-conf.el
+++ lib/packs/reasoning-pack/config/proof-general-conf.el
@@ -1,3 +1,4 @@
 (config/add-pack-lib "proofgeneral")
 
 (load-file (concat (config/pack-lib-dir) "proofgeneral/generic/proof-site.el"))
+(add-to-list 'proof-universal-keys '([(control j)] . proof-assert-next-command-interactive))
