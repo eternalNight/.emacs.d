a1e827d354d590b386d9ddf32fd7d78ff1078fc1
Tsin Smith <tsin.creator@gmail.com>
2013-11-07 14:05:47 +0800

    Add CMake mode
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/dev-pack/init.el
+++ lib/packs/dev-pack/init.el
@@ -1,4 +1,5 @@
 (config/load-config-file "auto-complete-conf.el")
+(config/load-config-file "cmake-conf.el")
 (config/load-config-file "eshell-conf.el")    ; Must invoked before eproject-conf
 (config/load-config-file "eproject-conf.el")
 (config/load-config-file "magit-conf.el")
