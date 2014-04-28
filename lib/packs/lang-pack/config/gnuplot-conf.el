ee63293ac59abd84eb3895b81a3f7a372de02727
Tsin Smith <tsin.creator@gmail.com>
2014-04-28 13:58:17 +0800

    Register *.gp to gnuplot-mode
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

---
+++ lib/packs/lang-pack/config/gnuplot-conf.el
@@ -0,0 +1,2 @@
+(autoload 'gnuplot-mode "gnuplot-mode" "Mode for Gnuplot" t)
+(add-to-list 'auto-mode-alist '("\\.gp$" . gnuplot-mode))
--- lib/packs/lang-pack/init.el
+++ lib/packs/lang-pack/init.el
@@ -3,6 +3,7 @@
 (config/load-config-file "c-mode-conf.el")
 (config/load-config-file "clojure-conf.el")
 (config/load-config-file "cocci-conf.el")
+(config/load-config-file "gnuplot-conf.el")
 (config/load-config-file "python-conf.el")
 (config/load-config-file "quack-conf.el")
 (config/load-config-file "tuareg-conf.el")
