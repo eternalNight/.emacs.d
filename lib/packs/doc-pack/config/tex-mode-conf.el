6ea7030275aec422f9c903d185abfa5135996d48
Tsin Smith <tsin.creator@gmail.com>
2014-04-28 13:59:19 +0800

    Add TeX-master configuration to eproject
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/doc-pack/config/tex-mode-conf.el
+++ lib/packs/doc-pack/config/tex-mode-conf.el
@@ -1,5 +1,11 @@
 (require 'auto-complete-auctex)
 
+(add-hook 'tex-generic-project-file-visit-hook
+	  (lambda ()
+	    (when (eproject-attribute :master)
+	      (let ((master (eproject-attribute :master)))
+		(setq TeX-master master)))))
+
 (add-hook 'LaTeX-mode-hook
 	  (lambda ()
 	    (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
