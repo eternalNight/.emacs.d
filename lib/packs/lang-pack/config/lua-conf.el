085962986cdd5105d6af72853fb34e75320df4dc
Tsin Smith <tsin.creator@gmail.com>
2014-10-20 09:41:42 +0800

    Add lua mode
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

---
+++ lib/packs/lang-pack/config/lua-conf.el
@@ -0,0 +1,5 @@
+(config/add-pack-lib "lua-mode")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
