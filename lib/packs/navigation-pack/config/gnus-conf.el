5ed3a310840a1b6a98ae02bd0b5d625da5ee113f
Tsin Smith <tsin.creator@gmail.com>
2015-01-12 10:28:32 +0800

    Switch to mailbox @yeah.net
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/navigation-pack/config/gnus-conf.el
+++ lib/packs/navigation-pack/config/gnus-conf.el
@@ -1,23 +1,29 @@
 (require 'gnus-desktop-notify)
 
+;; (setq gnus-select-method
+;;       '(nnimap "gmail"
+;; 	       (nnimap-inbox "INBOX")
+;; 	       (nnimap-address "imap.gmail.com")
+;; 	       (nnimap-server-port 993)
+;; 	       (nnimap-stream ssl)
+;; 	       (nnimap-fetch-partial-articles "text/")))
+
 (setq gnus-select-method
-      '(nnimap "gmail"
-	       (nnimap-inbox "INBOX")
-	       (nnimap-address "imap.gmail.com")
-	       (nnimap-server-port 993)
-	       (nnimap-stream ssl)
+      '(nnimap "yeah"
+	       (nnimap-address "imap.yeah.net")
+	       (nnimap-server-port 143)
+	       (nnimap-stream network)
 	       (nnimap-fetch-partial-articles "text/")))
 
 (add-to-list 'gnus-secondary-select-methods '(nntp "news.gmane.org"))
 
 (setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587
-				   "eternal.n08@gmail.com" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      tls-program '("D:/Git/bin/openssl.exe s_client -connect %h:%p -no_ssl2 -ign_eof")
+      smtpmail-starttls-credentials '(("smtp.yeah.net" 25 nil nil))
+      smtpmail-auth-credentials '(("smtp.yeah.net" 25
+				   "junjie_mao@yeah.net" nil))
+      smtpmail-default-smtp-server "smtp.yeah.net"
+      smtpmail-smtp-server "smtp.yeah.net"
+      smtpmail-smtp-service 25
       gnus-cache-directory (concat config/tmp-dir "Gnus/Mails/cache")
       mm-default-directory (concat config/tmp-dir "Gnus/MIME")
       gnus-read-active-file 'some
