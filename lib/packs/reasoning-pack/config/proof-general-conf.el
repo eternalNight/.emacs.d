5ce934f377a8a4c08c846a0af0d28e74da2c6895
Tsin Smith <tsin.creator@gmail.com>
2015-03-18 08:34:39 +0800

    Skip the splash buffer of proof general
    Signed-off-by: Tsin Smith <tsin.creator@gmail.com>

--- lib/packs/reasoning-pack/config/proof-general-conf.el
+++ lib/packs/reasoning-pack/config/proof-general-conf.el
@@ -2,3 +2,5 @@
 
 (load-file (concat (config/pack-lib-dir) "proofgeneral/generic/proof-site.el"))
 (add-to-list 'proof-universal-keys '([(control j)] . proof-assert-next-command-interactive))
+
+(setq proof-splash-enable nil)
